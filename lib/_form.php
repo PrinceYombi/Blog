22a033a709464c183d6b7ec107d7df238d7f538b
Prince Y <lightyombi2@gmail.com>
2024-08-26 23:33:12 +0200

    mise à jour

--- lib/_form.php
+++ lib/_form.php
@@ -1,14 +1,14 @@
 <form action="" method="post" enctype="multipart/form-data" class="form">
         <div class="form-line">
             <div class="form-label">Titre</div>
-            <input type="text" name="title" placeholder="Entrez le titre de votre article" value="<?php echo $article["title"]?>">
+            <input type="text" name="title" placeholder="Entrez le titre de votre article">
         </div>
         <div class="errors">
             <?php echo ($errors['title']) ? $errors["title"] : ""?>
         </div>
         <div class="form-line">
             <div class="form-label">Description</div>
-            <textarea name="description" id="" cols="30" rows="10"><?php echo $article["description"]?></textarea>
+            <textarea name="description" id="" cols="30" rows="10"></textarea>
         </div>
         <div class="errors">
            <?php echo ($errors['description']) ? $errors['description'] : ""?> 
